5db00152732a6eff983e7fb6c0297e875efff79c
Mikhail Savotikov <adaptedbee@yandex.ru>
2019-11-25 23:29:00 +0300

    Выносим список продуктов в компонент

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
+import ProductsList from '../ProductsList/ProductsList.js';
 import products from '../../products.json';
-import './Products.css';
 import ProductItem from 'csssr-school-product-card';
 
 const ratingComponent = ({ isFilled }) => {
   return <div className={isFilled ? "starFill" : ""} />;
 };
 
+const renderProduct = item => {
+  return <ProductItem
+    key={item.id}
+    isInStock={item.isInStock}
+    img={item.img}
+    title={item.title}
+    price={item.price}
+    subPriceContent={item.subPriceContent}
+    maxRating={item.maxRating}
+    rating={item.rating}
+    ratingComponent={ratingComponent}
+  />;
+}
+
 const PRODUCTS_LIMIT = 3;
 
 const Products = props => {
-  return <ul className="products">
-    {products.slice(0, PRODUCTS_LIMIT).map((item, index) => 
-      <ProductItem
-        key={item.id}
-        isInStock={item.isInStock}
-        img={item.img}
-        title={item.title}
-        price={item.price}
-        subPriceContent={item.subPriceContent}
-        maxRating={item.maxRating}
-        rating={item.rating}
-        ratingComponent={ratingComponent}
-      />
-    )}
-  </ul>;
+  return <ProductsList 
+    items={products} 
+    renderItem={renderProduct} 
+    limit={PRODUCTS_LIMIT} 
+  />;
 }
 
 export default Products;
---
+++ src/components/ProductsList/ProductsList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './ProductsList.css';
+
+const ProductsList = props => {
+  const productsLimit = props.limit ? props.limit : props.products.length;
+
+  return <ul className="products-list">
+    {props.items.slice(0, productsLimit).map((item, index) => 
+      props.renderItem(item)
+    )}
+  </ul>;
+}
+
+ProductsList.propTypes = {
+  items: PropTypes.array,
+  renderItem: PropTypes.func,
+  limit: PropTypes.number
+};
+
+export default ProductsList;
