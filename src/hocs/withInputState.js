6eebc4b23789dbac2d3f2c94e65d492ec0a86fcb
Mikhail Savotikov <adaptedbee@yandex.ru>
2019-12-26 03:17:57 +0300

    Правки - hoc для InputNumber

--- src/hocs/withInputState.js
+++ src/hocs/withInputState.js
@@ -2,20 +2,9 @@ import React from 'react';
 
 export default function withInputState(HoccedComponent) {
   return class extends React.Component {
-    // constructor(props) {
-    //   super(props);
-
-    //   this.state = {
-    //     value: 0
-    //   };
-    // }
-
     onChange = (event) => {
       const number = Number(event.target.value);
       
-      // this.setState({
-      //   value: number
-      // });
       this.props.onChange(number);
     }
 
@@ -23,7 +12,6 @@ export default function withInputState(HoccedComponent) {
       return (
         <HoccedComponent 
           {...this.props} 
-          // value={this.state.value}
           onChange={this.onChange} 
         />
       );
