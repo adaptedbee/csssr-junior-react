5c08bbf34d18b4d6ddabd8c0c17198917728d826
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-01-24 14:35:11 +0300

    Создаём стор и редьюсер

---
+++ src/store/reducers.js
@@ -0,0 +1,55 @@
+import { minBy, maxBy } from 'csssr-school-utils';
+
+import products from '../products.json';
+
+export function filtersReducer(state = {}, action) {
+  switch (action.type) {
+    case "UPDATE_PRICE": {
+      return Object.assign({}, state, {
+        filters: {
+          minPrice: action.data.minPrice,
+          maxPrice: action.data.maxPrice
+        }
+      });
+    }
+    case "UPDATE_DISCOUNT": {
+      return Object.assign({}, state, {
+        filters: {
+          discount: action.data.discount
+        }
+      });
+    }
+    case "UPDATE_CATEGORIES": {
+      let updatedCategories = [...state.filters.categories];
+      const categoryIndex = state.filters.categories.indexOf(action.data.category);
+      if (categoryIndex !== -1) {
+        updatedCategories.splice(categoryIndex, 1);
+      } else {
+        updatedCategories.push(category);
+      }
+
+      return Object.assign({}, state, {
+        filters: {
+          categories: updatedCategories
+        }
+      });
+
+      // const url = updatedCategories.join(',');
+      // window.history.pushState({ url }, 'title', url);
+    }
+    case "CLEAR_FILTERS": {
+      return Object.assign({}, state, {
+        filters: {
+          minPrice: minBy(obj => obj.price, products).price,
+          maxPrice: maxBy(obj => obj.price, products).price,
+          discount: 0,
+          categories: state.allCategories
+        }
+      });
+
+      // const url = this.state.allCategories.join(',');
+      // window.history.replaceState({ url }, 'title', url);
+    }
+  }
+  return state;
+}
