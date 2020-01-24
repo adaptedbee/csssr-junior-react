aad7e1e6a73f57c4a5fafec92ec694a041fa48ee
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-01-24 15:55:04 +0300

    ProductCard - выносим в компонент

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,36 +1,10 @@
 import React from 'react';
-import ProductItem from 'csssr-school-product-card';
-import { formatMoney } from 'csssr-school-utils';
 
 import LogRender from '../LogRender/LogRender.js';
 import List from '../List/List.js';
-import RatingComponent from '../RatingComponent/RatingComponent.js';
 import products from '../../products.json';
+import ProductCard from '../ProductCard/ProductCard.js';
 import FiltersContext from '../../filters-context';
-
-class ProductCard extends LogRender {
-  formatPrice = (number) => {
-    return formatMoney(number, 0, '.', ' ') + ' ₽';
-  }
-
-  render() {
-    const item = this.context.item;
-
-    return (
-      <ProductItem
-        key={item.id}
-        isInStock={item.isInStock}
-        img={item.img}
-        title={item.title}
-        price={this.formatPrice(item.price)}
-        subPriceContent={item.oldPrice ? this.formatPrice(item.oldPrice) : ''}
-        maxRating={item.maxRating}
-        rating={item.rating}
-        ratingComponent={RatingComponent}
-      />
-    );
-  }
-}
 
 class ProductsList extends LogRender {
   render() {
