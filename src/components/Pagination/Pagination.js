622499f67f9b23d7b9d86688fd7b6ce76afa5ff1
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-02-03 23:02:53 +0300

    Pagination - кнопки с номерами страниц

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -4,9 +4,12 @@ import PropTypes from 'prop-types';
 import './Pagination.css';
 
 class Pagination extends React.Component {
+  getPagesCount = () => {
+    return Math.ceil(this.props.productsCount/this.props.productsPerPage);
+  }
+
   goToPage = (page) => {
-    const pagesCount = Math.ceil(this.props.productsCount/this.props.productsPerPage);
-    if (page <= 0 || page > pagesCount || page === this.props.currentPage) {
+    if (page <= 0 || page > this.getPagesCount() || page === this.props.currentPage) {
       return;
     }
     this.props.goToPage(page);
@@ -23,32 +26,53 @@ class Pagination extends React.Component {
             Назад
           </button>
         </li>
-  
+
+        {this.props.currentPage > 1 ? (
+          <li className="pagination__item">
+            <button 
+              onClick={() => this.goToPage(this.props.currentPage - 1)}
+              className="pagination__button"
+            >
+              {this.props.currentPage - 1}
+            </button>
+          </li>
+        ) : ''}
         <li className="pagination__item">
-          <button
-            className={"pagination__button " + (this.props.currentPage === 1 ? 'pagination__button--active' : '')}
-          >
-            1
+          <button className="pagination__button pagination__button--active">
+            {this.props.currentPage}
           </button>
         </li>
-        <li className="pagination__item">
-          <button className="pagination__button">2</button>
-        </li>
-        <li className="pagination__item">
-          <button className="pagination__button">3</button>
-        </li>
-        <li className="pagination__item">
-          <button className="pagination__button">4</button>
-        </li>
-        <li className="pagination__item">
-          <button className="pagination__button">5</button>
-        </li>
-        <li className="pagination__item">
-          <button className="pagination__button">...</button>
-        </li>
-        <li className="pagination__item">
-          <button className="pagination__button">13</button>
-        </li>
+        {this.props.currentPage < this.getPagesCount() ? (
+          <li className="pagination__item">
+            <button 
+              className="pagination__button"
+              onClick={() => this.goToPage(this.props.currentPage + 1)}
+            >
+              {this.props.currentPage + 1}
+            </button>
+          </li>
+        ) : ''}
+        
+        {this.props.currentPage < this.getPagesCount() - 2 ? (
+          <li className="pagination__item">
+            <button 
+              className="pagination__button"
+              onClick={() => this.goToPage(this.props.currentPage + 2)}
+            >
+              ...
+            </button>
+          </li>
+        ) : ''}
+        {this.props.currentPage < this.getPagesCount() - 1 ? (
+          <li className="pagination__item">
+            <button 
+              className="pagination__button"
+              onClick={() => this.goToPage(this.getPagesCount())}
+            >
+              {this.getPagesCount()}
+            </button>
+          </li>
+        ) : ''}
   
         <li className="pagination__item pagination__item--next">
           <button 
