7cbde29691cbb5bbd52cef12853df2b642dd7e9d
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-02-18 01:48:03 +0300

    Страница товара - условие отсутствия товара

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -15,49 +15,49 @@ const ProductPage = ({product}) => {
           <span className="visually-hidden">Назад в каталог</span>
         </a>
         <h1 className="page__headline">
-          {product.title}
+          {product ? product.title : 'Товар не найден'}
         </h1>
       </header>
 
-      <section 
-        className={product.isInStock ? 'product' : 'product product--out-of-stock'}>
-        <div 
-          className={product.isInStock ? 'product__status' : 'product__status product__status--out-of-stock'}>
-          {product.isInStock ? 'В наличии' : 'Недоступен'}
-        </div>
-        <img
-          className="product__image"
-          src={product.img}
-          alt={product.title}
-          width="224"
-          height="200"
-        />
-        <div className="product__info">
-          <h2 className="product__title">
-            {product.title}
-          </h2>
-          <div className="product__rating-wrapper">
-            {range(product.maxRating).map(i => 
-              <RatingComponent key={i} isFilled={i <= product.rating} />
-            )}
+      {product ? (
+        <section className={product.isInStock ? 'product' : 'product product--out-of-stock'}>
+          <div className={product.isInStock ? 'product__status' : 'product__status product__status--out-of-stock'}>
+            {product.isInStock ? 'В наличии' : 'Недоступен'}
           </div>
-          <div className="product__price-wrapper">
-            <p className="product__price">
-              {formatPrice(product.price)}
-            </p>
-            <p className="product__subprice">
-              {product.oldPrice ? formatPrice(product.oldPrice) : ''}
-            </p>
+          <img
+            className="product__image"
+            src={product.img}
+            alt={product.title}
+            width="224"
+            height="200"
+          />
+          <div className="product__info">
+            <h2 className="product__title">
+              {product.title}
+            </h2>
+            <div className="product__rating-wrapper">
+              {range(product.maxRating).map(i => 
+                <RatingComponent key={i} isFilled={i <= product.rating} />
+              )}
+            </div>
+            <div className="product__price-wrapper">
+              <p className="product__price">
+                {formatPrice(product.price)}
+              </p>
+              <p className="product__subprice">
+                {product.oldPrice ? formatPrice(product.oldPrice) : ''}
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
-
-      <img 
-        className="page__not-found-image" 
-        src="img/planet-lamp.svg"
-        width="512"
-        height="512"
-        alt="Товар не найден" />
+        </section>
+      ) : (
+        <img 
+          className="page__not-found-image" 
+          src="img/planet-lamp.svg"
+          width="512"
+          height="512"
+          alt="Товар не найден" />
+      )}
     </article>
   );
 }
