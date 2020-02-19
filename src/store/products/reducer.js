c139b885731c5a0dee352ef9c36c56fb0c8fa684
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-02-20 00:56:33 +0300

    Селектор для категорий

--- src/store/products/reducer.js
+++ src/store/products/reducer.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import queryString from 'query-string';
 
 import products from '../../products.json';
 import { getFilters } from '../filters/reducer';
@@ -17,13 +18,25 @@ export default function productsReducer(state = initialState, action) {
 
 export const getProducts = (state) => state.products.products;
 
+// export const getLocation = (state) => state.router.location;
+export const getCategories = (state) => {
+  let categories = [];
+  const params = queryString.parse(state.router.location.search);
+  const categoriesParam = params.categories;
+  if (categoriesParam) {
+    categories = categoriesParam.split(',');
+  }
+
+  return categories;
+}
+
 export const getFilteredProducts = createSelector(
-  [getFilters, getProducts],
-  (filters, products) => {
+  [getFilters, getProducts, getCategories],
+  (filters, products, categories) => {
     const filteredProducts = products
       .filter(item => item.price >= filters.minPrice && item.price <= filters.maxPrice)
       .filter(item => filters.discount === 0 || (item.oldPrice && (item.oldPrice/item.price) - 1 >= filters.discount/100))
-      .filter(item => filters.categories.includes(item.category));
+      .filter(item => categories.includes(item.category));
   
     return filteredProducts;
   }
