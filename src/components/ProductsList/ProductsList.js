f8a1b49655d176444ba11437b58f28b0780728b4
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-03-09 18:46:31 +0300

    ProductsList - отображаем разные состояния при загрузке продуктов

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -6,20 +6,92 @@ import LogRender from '../LogRender/LogRender.js';
 import List from '../List/List.js';
 import ProductCard from '../ProductCard/ProductCard.js';
 import Pagination from '../Pagination/Pagination';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage.js';
+
+const FETCH_PRODUCTS_URL = 'https://course-api.csssr.school/products';
 
 class ProductsList extends LogRender {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      isError: false,
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchProducts(FETCH_PRODUCTS_URL);
+  }
+
+  fetchProducts = (url) => {
+    this.setState({
+      isLoading: true
+    });
+
+    setTimeout(() => {
+      fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response);
+        };
+      })
+      .then(result => {
+        if (result && result.result === 'OK' && result.products) {
+          this.setState({
+            isLoading: false,
+            isError: false,
+            products: result.products
+          });
+        }
+        if (result && result.result === 'ERROR' && result.message) {
+          throw new Error(result.message);
+        }
+      })
+      .catch(error => {
+        this.setState({
+          isLoading: false,
+          isError: true
+        });
+      })
+    }, 3000);
+  }
+
   render() {
     return (
       <React.Fragment>
-        <List items={this.props.productsOnPage} renderItem={ProductCard} />
+        {this.state.isLoading && !this.state.isError ? (
+          <img 
+            className="page__image" 
+            src="/img/loading-items.svg"
+            width="736"
+            height="648"
+            alt="Загрузка..." />
+        ) : ''}
 
-        {this.props.productsOnPage.length > 0 ? (
-          <Pagination 
-            currentPage={this.props.currentPage}
-            productsPerPage={this.props.productsPerPage}
-            productsCount={this.props.filteredProducts.length}
-            urlSearchParams={this.props.urlSearchParams}
-          />
+        {!this.state.isLoading && !this.state.isError && this.state.products && this.state.products.length > 0 ? (
+          <React.Fragment>
+            <List items={this.props.productsOnPage} renderItem={ProductCard} />
+
+            {this.props.productsOnPage.length > 0 ? (
+              <Pagination 
+                currentPage={this.props.currentPage}
+                productsPerPage={this.props.productsPerPage}
+                productsCount={this.props.filteredProducts.length}
+                urlSearchParams={this.props.urlSearchParams}
+              />
+            ) : ''}
+          </React.Fragment>
+        ) : ''}
+
+        {!this.state.isLoading && !this.state.isError && (!this.state.products || this.state.products.length === 0) ? (
+          <NotFoundPage headline={'Товары не найдены'} showBackLink={false} />
+        ) : ''}
+
+        {!this.state.isLoading && this.state.isError ? (
+          <NotFoundPage headline={'Произошла ошибка'} showBackLink={false} />
         ) : ''}
       </React.Fragment>
     );
